55bebb28d2a6e57d98b8cde26b333d762c42e044
Wojciech Ciok <33331400+wojciechciok@users.noreply.github.com>
2022-05-10 16:06:22 +0200

    added favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,19 @@ import HistorySection from "@/components/Sections/HistorySection";
 import ImpactSection from "@/components/Sections/ImpactSection";
 import WhyCareSection from "@/components/Sections/WhyCareSection";
 import SourcesSection from "@/components/Sections/SourcesSection";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Layout>
+        <Head>
+          <title>CO2 Global Overview</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <div className="grid h-screen text-center text-7xl font-semibold">
           <div className="self-center">
             Overview and impact of CO2 emissions in the world 🏭
